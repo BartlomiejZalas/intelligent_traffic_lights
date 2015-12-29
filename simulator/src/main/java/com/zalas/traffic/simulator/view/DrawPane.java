f5581588c80a99b58c56a6036a9a0f431b067347
Bartlomiej Zalas <bartekzalas@gmail.com>
2015-12-29 19:17:50 +0100

    drawing traffic arrows

--- simulator/src/main/java/com/zalas/traffic/simulator/view/DrawPane.java
+++ simulator/src/main/java/com/zalas/traffic/simulator/view/DrawPane.java
@@ -1,9 +1,15 @@
 package com.zalas.traffic.simulator.view;
 
+import com.zalas.traffic.simulator.model.LightCycle;
+import com.zalas.traffic.simulator.model.TrafficDirection;
+import com.zalas.traffic.simulator.model.TrafficFlow;
 import com.zalas.traffic.simulator.model.TrafficModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
+
+import static com.zalas.traffic.simulator.model.TrafficDirection.*;
 
 public class DrawPane extends JPanel {
 
@@ -29,6 +35,35 @@ public class DrawPane extends JPanel {
 
         drawTrafficStatus(g2d, width, height, mediumFontSize);
 
+        drawTrafficFlows(g2d, width, height);
+
+    }
+
+    private void drawTrafficFlows(Graphics2D g2d, int width, int height) {
+        int marginX = width / 3;
+        int marginY = height / 3;
+
+        int midWidthX = width / 2;
+        int midWidthY = height / 2;
+
+        HashMap<TrafficDirection, int[]> directionStaringPoints = new HashMap<>();
+        directionStaringPoints.put(NORTH, new int[]{midWidthX, marginY});
+        directionStaringPoints.put(EAST, new int[]{width - marginX, midWidthY});
+        directionStaringPoints.put(SOUTH, new int[]{midWidthX, height - marginY});
+        directionStaringPoints.put(WEST, new int[]{marginX, midWidthY});
+
+        LightCycle lc = trafficModel.getLightCycle();
+
+        if (lc != null) {
+            for (TrafficFlow tf : lc.getTrafficFlows()) {
+
+                int x1 = directionStaringPoints.get(tf.getFrom())[0];
+                int y1 = directionStaringPoints.get(tf.getFrom())[1];
+                int x2 = directionStaringPoints.get(tf.getTo())[0];
+                int y2 = directionStaringPoints.get(tf.getTo())[1];
+                drawArrowLine(g2d, x1, y1, x2, y2, 20, 10);
+            }
+        }
     }
 
     private void drawTrafficStatus(Graphics2D g2d, int width, int height, int fontSize) {
@@ -53,5 +88,28 @@ public class DrawPane extends JPanel {
 
     private Color getRoadColor() {
         return Color.DARK_GRAY;
+    }
+
+    private void drawArrowLine(Graphics2D g, int x1, int y1, int x2, int y2, int d, int h) {
+        int dx = x2 - x1, dy = y2 - y1;
+        double D = Math.sqrt(dx * dx + dy * dy);
+        double xm = D - d, xn = xm, ym = h, yn = -h, x;
+        double sin = dy / D, cos = dx / D;
+
+        x = xm * cos - ym * sin + x1;
+        ym = xm * sin + ym * cos + y1;
+        xm = x;
+
+        x = xn * cos - yn * sin + x1;
+        yn = xn * sin + yn * cos + y1;
+        xn = x;
+
+        int[] xpoints = {x2, (int) xm, (int) xn};
+        int[] ypoints = {y2, (int) ym, (int) yn};
+
+        g.setColor(Color.GREEN);
+        g.setStroke(new BasicStroke(2));
+        g.drawLine(x1, y1, x2, y2);
+        g.fillPolygon(xpoints, ypoints, 3);
     }
 }
